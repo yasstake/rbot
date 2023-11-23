53d397e0786387773802c780819190ed90ac9410
yasstake <yasstake@users.noreply.github.com>
2023-11-24 01:26:36 +0900

    Fix board.get

--- src/exchange/orderbook.rs
+++ src/exchange/orderbook.rs
@@ -1,14 +1,19 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Mutex};
 
 use ndarray::Array2;
-use numpy::{PyArray2, IntoPyArray};
-use polars_core::{series::Series, prelude::{DataFrame, NamedFrom}};
-use pyo3::{PyResult, Py, Python, pyclass};
+use numpy::{IntoPyArray, PyArray2};
+use polars_core::{
+    prelude::{DataFrame, NamedFrom},
+    series::Series,
+};
+use pyo3::{pyclass, Py, PyResult, Python};
 use pyo3_polars::PyDataFrame;
 use rust_decimal::prelude::*;
 use rust_decimal_macros::dec;
-use serde::{Deserializer, de, Deserialize};
+use serde::{de, Deserialize, Deserializer};
 use serde_derive::{Deserialize, Serialize};
+
+use crate::{common::MarketConfig, exchange::binance::Market};
 
 pub fn string_to_f64<'de, D>(deserializer: D) -> Result<f64, D::Error>
 where
@@ -43,8 +48,6 @@ where
     }
 }
 
-
-
 #[pyclass]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct BoardItem {
@@ -72,16 +75,16 @@ impl BoardItem {
 
 #[derive(Debug)]
 pub struct Board {
-    step: Decimal,
     asc: bool,
+    max_depth: u32,
     board: HashMap<Decimal, Decimal>,
 }
 
 impl Board {
-    pub fn new(step: Decimal, asc: bool) -> Self {
+    pub fn new(config: &MarketConfig, asc: bool) -> Self {
         Board {
-            step,
             asc,
+            max_depth: config.board_depth,
             board: HashMap::new(),
         }
     }
@@ -95,19 +98,10 @@ impl Board {
         self.board.insert(price, size);
     }
 
-    fn step(&self) -> Decimal {
-        if self.asc {
-            self.step
-        } else {
-            -self.step
-        }
-    }
-
-    // Keyをソートして、Vecにして返す
-    // ascがtrueなら昇順、falseなら降順
-    // stepサイズごとで0の値も含めて返す
-    // stepサイズが０のときは、stepサイズを無視して返す
-    pub fn get(&self) -> Vec<BoardItem> {
+    /// Keyをソートして、Vecにして返す
+    /// ascがtrueなら昇順、falseなら降順
+    /// max_depthを超えたものは削除する.
+    pub fn get(&mut self) -> Vec<BoardItem> {
         let mut vec: Vec<BoardItem> = Vec::from_iter(
             self.board
                 .iter()
@@ -120,73 +114,32 @@ impl Board {
             vec.sort_by(|a, b| b.price.partial_cmp(&a.price).unwrap());
         }
 
-        vec
-    }
-
-    pub fn get_with_fill(&self) -> Vec<BoardItem> {
-        let mut v: Vec<BoardItem> = vec![];
-        let mut current_price = dec!(0.0);
-
-        for item in self.get() {
-            if current_price == dec!(0.0) {
-                current_price = item.price;
+        if self.max_depth != 0 && self.max_depth < vec.len() as u32 {
+            log::info!("board depth over. remove items.");
+            let not_valid = vec.split_off(self.max_depth as usize);
+            for item in not_valid {
+                self.board.remove(&item.price);
             }
-
-            while current_price != item.price {
-                v.push(BoardItem::from_decimal(current_price, dec!(0.0)));
-                current_price += self.step();
-            }
-
-            v.push(BoardItem::from_decimal(item.price, item.size));
-            current_price += self.step();
+            self.board.shrink_to_fit();
         }
 
-        v
-    }
-
-    pub fn get_array(&self) -> Array2<f64> {
-        return Self::to_ndarray(&self.get());
+        vec
     }
 
     pub fn clear(&mut self) {
         self.board.clear();
     }
 
-    // convert to ndarray
-    pub fn to_ndarray(board: &Vec<BoardItem>) -> Array2<f64> {
-        let shape = (board.len(), 2);
-        let mut array_vec: Vec<f64> = Vec::with_capacity(shape.0 * shape.1);
-
-        for item in board {
-            array_vec.push(item.price.to_f64().unwrap());
-            array_vec.push(item.size.to_f64().unwrap());
-        }
-
-        let array: Array2<f64> = Array2::from_shape_vec(shape, array_vec).unwrap();
-
-        array
-    }
-
-    pub fn to_pyarray(&self) -> PyResult<Py<PyArray2<f64>>> {
-        let array = self.get_array();
-        let r = Python::with_gil(|py| {
-            let py_array2: &PyArray2<f64> = array.into_pyarray(py);
-            py_array2.to_owned()
-        });
-
-        return Ok(r);
-    }
-
-    pub fn to_pydataframe(&self) -> PyResult<PyDataFrame> {
+    pub fn to_dataframe(&mut self) -> Result<DataFrame, ()> {
         let board = self.get();
 
         let mut prices: Vec<f64> = vec![];
-        let mut sizes: Vec<f64> = vec![];        
+        let mut sizes: Vec<f64> = vec![];
         let mut cusum_col: Vec<f64> = vec![];
         let mut cusum: Decimal = dec![0.0];
 
         for item in board {
-            prices.push(item.price.to_f64().unwrap());   
+            prices.push(item.price.to_f64().unwrap());
             sizes.push(item.size.to_f64().unwrap());
             cusum += item.size;
             cusum_col.push(cusum.to_f64().unwrap());
@@ -198,21 +151,21 @@ impl Board {
 
         let df = DataFrame::new(vec![prices, sizes, cusum]).unwrap();
 
-        Ok(PyDataFrame(df))
+        Ok(df)
     }
 }
 
 #[derive(Debug)]
-struct Depth {
+pub struct OrderBookRaw {
     bids: Board,
     asks: Board,
 }
 
-impl Depth {
-    pub fn new(step: Decimal) -> Self {
-        Depth {
-            bids: Board::new(step, false),
-            asks: Board::new(step, true),
+impl OrderBookRaw {
+    pub fn new(config: &MarketConfig) -> Self {
+        OrderBookRaw {
+            bids: Board::new(config, false),
+            asks: Board::new(config, true),
         }
     }
 
@@ -221,97 +174,74 @@ impl Depth {
         self.asks.clear();
     }
 
-    pub fn get_asks(&self) -> Vec<BoardItem> {
-        self.asks.get()
+    pub fn get_asks_pydataframe(&mut self) -> Result<DataFrame, ()> {
+        self.asks.to_dataframe()
     }
 
-    pub fn get_asks_pyarray(&self) -> PyResult<Py<PyArray2<f64>>> {
-        self.asks.to_pyarray()
+    pub fn get_bids_pydataframe(&mut self) -> Result<DataFrame, ()> {
+        self.bids.to_dataframe()
     }
 
-    pub fn get_asks_pydataframe(&self) -> PyResult<PyDataFrame> {
-        self.asks.to_pydataframe()
-    }
+    pub fn update(&mut self, bids_diff: &Vec<BoardItem>, asks_diff: &Vec<BoardItem>, force: bool) {
+        if force {
+            self.clear();
+        }
 
-    pub fn get_bids(&self) -> Vec<BoardItem> {
-        self.bids.get()
-    }
+        for item in bids_diff {
+            self.bids.set(item.price, item.size);
+        }
 
-    pub fn get_bids_pyarray(&self) -> PyResult<Py<PyArray2<f64>>> {
-        self.bids.to_pyarray()
-    }
-
-    pub fn get_bids_pydataframe(&self) -> PyResult<PyDataFrame> {
-        self.bids.to_pydataframe()
-    }
-
-    pub fn set_bids(&mut self, price: Decimal, size: Decimal) {
-        self.bids.set(price, size);
-    }
-
-    pub fn set_asks(&mut self, price: Decimal, size: Decimal) {
-        self.asks.set(price, size);
+        for item in asks_diff {
+            self.asks.set(item.price, item.size);
+        }
     }
 }
 
 #[derive(Debug)]
 pub struct OrderBook {
-    symbol: String,
-    depth: Depth,
+    board: Mutex<OrderBookRaw>,
 }
 
 impl OrderBook {
-    pub fn new(symbol: String, step: Decimal) -> Self {
+    pub fn new(config: &MarketConfig) -> Self {
         OrderBook {
-            symbol: symbol,
-            depth: Depth::new(step),
+            board: Mutex::new(OrderBookRaw::new(config)),
         }
     }
 
-    pub fn update(&mut self, bids_diff: &Vec<BoardItem>, asks_diff: &Vec<BoardItem>, force: bool) {
-        if force {
-            self.depth.clear();
-        }
-
-        for item in bids_diff {
-            self.depth.set_bids(item.price, item.size);
-        }
-
-        for item in asks_diff {
-            self.depth.set_asks(item.price, item.size);
-        }
+    pub fn clear(&mut self) {
+        self.board.lock().unwrap().clear();
     }
 
-    pub fn get_bids(&self) -> Vec<BoardItem> {
-        self.depth.bids.get()
-    }
-
-    pub fn get_bids_pyarray(&self) -> PyResult<Py<PyArray2<f64>>> {
-        self.depth.bids.to_pyarray()
+    /*
+    pub fn get_asks_pydataframe(&self) -> PyResult<PyDataFrame> {
+        self.board.lock().unwrap().get_asks_pydataframe()
     }
 
     pub fn get_bids_pydataframe(&self) -> PyResult<PyDataFrame> {
-        self.depth.bids.to_pydataframe()
+        self.board.lock().unwrap().get_bids_pydataframe()
+    }
+    */
+
+    pub fn get_board(&self) -> Result<(DataFrame, DataFrame), ()> {
+        let mut board = self.board.lock().unwrap();
+        let bids = board.get_bids_pydataframe()?;
+        let asks = board.get_asks_pydataframe()?;
+        Ok((bids, asks))
     }
 
-    pub fn get_asks(&self) -> Vec<BoardItem> {
-        self.depth.asks.get()
+    pub fn update(&mut self, bids_diff: &Vec<BoardItem>, asks_diff: &Vec<BoardItem>, force: bool) {
+        self.board.lock().unwrap().update(bids_diff, asks_diff, force);
     }
-
-    pub fn get_asks_pyarray(&self) -> PyResult<Py<PyArray2<f64>>> {
-        self.depth.asks.to_pyarray()
-    }
-
-    pub fn get_asks_pydataframe(&self) -> PyResult<PyDataFrame> {
-        self.depth.asks.to_pydataframe()
-    }
-    
 }
 
 
 #[test]
 fn test_board_set() {
-    let mut b = Board::new(dec!(0.5), true);
+    let mut config = MarketConfig::new("USD", "JPY", 2, 2);
+    config.price_unit = dec!(0.5);
+
+    let mut b = Board::new(&config, true);
 
     b.set(dec!(10.0), dec!(1.0));
     println!("{:?}", b.get());
@@ -325,7 +255,7 @@ fn test_board_set() {
     b.set(dec!(9.0), dec!(0.0));
     println!("{:?}", b.get());
 
-    let mut b = Board::new(dec!(0.5), false);
+    let mut b = Board::new(&config, false);
 
     println!("---------desc----------");
 
@@ -344,14 +274,4 @@ fn test_board_set() {
     println!("---------clear----------");
     b.clear();
     println!("{:?}", b.get());
-}
-
-#[test]
-fn to_ndarray() {
-    let mut b = Board::new(dec!(0.5), true);
-    b.set(dec!(10.0), dec!(1.0));
-    b.set(dec!(12.5), dec!(1.0));
-
-    let array = b.get_array();
-    println!("{:?}", array);
 }
