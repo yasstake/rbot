420a629ce7d91526ef31e7c0e05ea854705fb2e8
yasstake <yasstake@users.noreply.github.com>
2024-03-19 23:43:12 +0900

    Add db root for rbot

--- rbot_lib/src/db/fs.rs
+++ rbot_lib/src/db/fs.rs
@@ -2,40 +2,45 @@
 
 use std::fs;
 use std::path::PathBuf;
+use std::sync::Mutex;
 use directories::ProjectDirs;
 use once_cell::sync::Lazy;
+use pyo3::pyfunction;
 
 use crate::common::env_rbot_db_root;
 
 
-pub static DB_ROOT: Lazy<String> = Lazy::new(|| {
+#[pyfunction]
+pub fn get_db_root() -> String {
+    DB_ROOT.lock().unwrap().to_string()
+}
+
+#[pyfunction]
+pub fn set_db_root(path: &str) {
+    let mut root_path = DB_ROOT.lock().unwrap();
+
+    *root_path = path.to_string();
+}
+
+pub static DB_ROOT: Lazy<Mutex<String>> = Lazy::new(|| Mutex::new({
     if let Ok(path) = env_rbot_db_root() {
-        return path;
+        path
     } else {
-        "".to_string()
+        project_dir()        
     }
-});
+}));
 
 
-pub fn project_dir() -> PathBuf {
+pub fn project_dir() -> String {
     let proj_dir = ProjectDirs::from("net", "takibi", "rbot").unwrap();
 
-    return proj_dir.data_dir().to_owned();
+    return proj_dir.data_dir().to_str().unwrap().to_string();
 }
 
 pub fn db_full_path(exchange_name: &str, category: &str, symbol: &str, base_dir: &str, test_net: bool) -> PathBuf {
-    let path = DB_ROOT.to_string();    
+    let project_dir = get_db_root();
+    let project_dir = PathBuf::from(project_dir);
     
-    let project_dir = if base_dir != "" {
-        PathBuf::from(base_dir)
-    }
-    else if path != "" {
-        PathBuf::from(path)
-    }
-    else {
-        project_dir()
-    };
-
     let db_dir = project_dir.join("DB");
     let exchange_dir = db_dir.join(exchange_name);
     let _ = fs::create_dir_all(&exchange_dir);
@@ -58,6 +63,7 @@ mod test_fs {
     #[test]
     fn test_project_dir() {
         let path = project_dir();
+        let path = PathBuf::from(path);
 
         let db_name = path.join(".db");
 
