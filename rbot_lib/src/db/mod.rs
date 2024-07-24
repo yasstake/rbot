898b185822147240a3089308e289825cc04c2c03
yass <yass@takibi.net>
2024-07-24 09:42:09 +0900

    Delete avo, *.gz.csv archive format

--- rbot_lib/src/db/mod.rs
+++ rbot_lib/src/db/mod.rs
@@ -4,15 +4,10 @@ pub mod sqlite;
 pub mod df;
 pub mod fs;
 pub mod archive;
-pub mod compress;
-pub mod avro;
 
 pub use sqlite::*;
 pub use df::*;
 pub use fs::*;
 pub use archive::*;
-pub use compress::*;
-pub use avro::*;
-
 
 
