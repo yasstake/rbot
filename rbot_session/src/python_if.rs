dcfd2e87140e132e1ea1e38a7184dcbc8a7e7a76
yass <yass@takibi.net>
2024-07-25 23:09:43 +0900

    Agent param is now Bound<PyAny> instead PyAny

--- rbot_session/src/python_if.rs
+++ rbot_session/src/python_if.rs
@@ -1,10 +1,12 @@
 // Copyright(c) 2022-2024. yasstake. All rights reserved.
 
-use pyo3::PyAny;
+use pyo3::{types::PyAnyMethods, Bound, PyAny};
 
 /// Checks if a given Python object has a method with the specified name.
-pub fn has_method(agent: &PyAny, method_name: &str) -> bool {
-    if agent.dir().contains(method_name).unwrap_or(false) {
+pub fn has_method(agent: &Bound<PyAny>, method_name: &str) -> bool {
+    let dir  = agent.dir();
+
+    if dir.contains(method_name).unwrap_or(false) {
         return true;
     } else {
         return false;
