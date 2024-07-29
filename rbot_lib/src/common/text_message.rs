90d66c06536a52e1edd2b066f36313a4b5e055a7
yass <yass@takibi.net>
2024-07-29 22:44:33 +0900

    Change profit to total_profit, move bar imple to rbot_lib

--- rbot_lib/src/common/text_message.rs
+++ rbot_lib/src/common/text_message.rs
@@ -1,39 +1,29 @@
-
-use std::sync::Mutex;
 use once_cell::sync::Lazy;
+use std::sync::Mutex;
 
-const BUFFER_SIZE: usize = 4096;
-
-static MESSAGE_BUFFER: Lazy<Mutex<Vec<String>>> =
-Lazy::new(|| Mutex::new(Vec::<String>::new()));
+static MESSAGE_BUFFER: Lazy<Mutex<Vec<String>>> = Lazy::new(|| Mutex::new(Vec::<String>::new()));
 
 pub fn write_agent_messsage(message: &str) {
     let mut lock = MESSAGE_BUFFER.lock().unwrap();
 
-    println!("set: {}", message);
-
     lock.push(message.to_string());
-    if BUFFER_SIZE < lock.len(){
-        lock.pop();
-    }
 }
 
 pub fn get_agent_message() -> Vec<String> {
     let mut lock = MESSAGE_BUFFER.lock().unwrap();
-   
-    let message = lock.clone();
-    lock.clear();    
 
-    if lock.len() != 0 {    
+    let message = lock.clone();
+    lock.clear();
+
+    if lock.len() != 0 {
         println!("GET!! {:?}", message);
     }
 
     message
 }
 
-
 #[cfg(test)]
-mod test_message_write{
+mod test_message_write {
     use super::{get_agent_message, write_agent_messsage};
 
     #[test]
@@ -44,6 +34,4 @@ mod test_message_write{
         let msg = get_agent_message();
         println!("{:?}", msg);
     }
-
-
 }
