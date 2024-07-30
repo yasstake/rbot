8de7a2d3757d387b1554d43e3b5eabb41bd1ba74
yass <yass@takibi.net>
2024-07-30 19:11:58 +0900

    revert not to use range description

--- rbot_lib/src/common/calc_class.rs
+++ rbot_lib/src/common/calc_class.rs
@@ -1,9 +1,6 @@
-use rust_decimal::{
-    prelude::{FromPrimitive, ToPrimitive},
-    Decimal,
-};
-
-use super::{format_number, MarketConfig};
+use rust_decimal::{prelude::FromPrimitive, Decimal};
+use super::format_number;
+use super::MarketConfig;
 
 pub fn calc_class(config: &MarketConfig, profit: f64, duration_min: i64) -> String {
     if duration_min < 1 {
@@ -12,51 +9,60 @@ pub fn calc_class(config: &MarketConfig, profit: f64, duration_min: i64) -> Stri
 
     let monthly_profit = profit * 30.0 * 24.0 * 60.0 / duration_min as f64;
 
-    let profit: i64 = profit.floor() as i64;
-    let monthly_profit: i64 = monthly_profit.floor() as i64;
-
     let class_string = match config.home_currency.to_uppercase().as_str() {
         "USD" | "USDT" | "USDC" => {
-            if monthly_profit < -1_000_000 {
+            if monthly_profit < -1_000_000.0 {
                 // -ss
                 "class -ss"
+            } else if monthly_profit < -100_000.0 {
+                // -s
+                "class -s"
+            } else if monthly_profit < -10_000.0 {
+                // -a
+                "class -a"
+            } else if monthly_profit < -1_000.0 {
+                // -b
+                "class -b"
+            } else if monthly_profit < -100.0 {
+                // -c
+                "class -c"
+            } else if monthly_profit < 0.0 {
+                // -
+                "class -"
+            } else if monthly_profit < 100.0 {
+                // +
+                "class +"
+            } else if monthly_profit < 1_000.0 {
+                // class C
+                "CLASS C"
+            } else if monthly_profit < 10_000.0 {
+                // class B
+                "CLASS B"
+            } else if monthly_profit < 100_000.0 {
+                // class A
+                "CLASS A"
+            } else if monthly_profit < 1_000_000.0 {
+                // class SS
+                "CLASS S"
             } else {
-                match monthly_profit {
-                    -1_000_000..-100_000 => "class -s",
-                    -100_000..-10_000 => "class -a",
-                    -10_000..-1_000 => "class -b",
-                    -1_000..-100 => "class -c",
-                    -100..0 => "class -",
-                    0..100 => "class +",
-                    100..1_000 => "CLASS C",
-                    1_000..10_000 => "CLASS B",
-                    10_000..1_000_000 => "CLASS A",
-                    _ => {
-                        if 1_000_000 < monthly_profit {
-                            "CLASS SS"
-                        } else {
-                            println!("unknown class {}", monthly_profit);
-                            ""
-                        }
-                    }
-                }
+                "CLASS SS"
             }
         }
-        _ => {""}
+        _ => "",
     };
 
+    let monthly_profit = monthly_profit.floor() as i64;
 
-
-    return format!(
-        "PROFIT: {:6}[{}]/({}[min]={:.2}[Month]) => {:6}[{}]/Month [{}]",
-        format_number(profit),
-        config.home_currency,
-        format_number(duration_min),
-        duration_min as f64 / 30.0 / 24.0 / 60.0,
-        format_number(monthly_profit),
-        config.home_currency,
-        class_string
-    );
+        return format!(
+            "PROFIT: {:>6}[{}]/({}[min]={:.2}[Month]) => {:>}[{}]/Month [{}]",
+            format_number(profit as i64),
+            config.home_currency,
+            format_number(duration_min),
+            duration_min as f64 / 30.0 / 24.0 / 60.0,
+            format_number(monthly_profit),
+            config.home_currency,
+            class_string
+        );
 }
 
 #[cfg(test)]
